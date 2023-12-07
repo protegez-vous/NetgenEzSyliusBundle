6d5f3699c402c969cfbb5e45a8e1040a6924363e
pkamps <pkamps@mugo.ca>
2023-12-07 16:06:56 +0100

    Update NetgenEzSyliusExtension.php

--- DependencyInjection/NetgenEzSyliusExtension.php
+++ DependencyInjection/NetgenEzSyliusExtension.php
@@ -38,7 +38,7 @@ class NetgenEzSyliusExtension extends Extension implements PrependExtensionInter
     {
         $configFile = __DIR__ . '/../Resources/config/ezplatform.yml';
         $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ezpublish', $config);
+        $container->prependExtensionConfig('ibexa', $config);
         $container->addResource(new FileResource($configFile));
     }
 }
